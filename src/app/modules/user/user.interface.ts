5e0d0e358c2813bc9f5e2df028ab18065a17ffcb
rownakabdullahomi <rownakabdullahomi@gmail.com>
2025-07-09 11:27:01 +0600

    user interface created

---
+++ src/app/modules/user/user.interface.ts
@@ -0,0 +1,39 @@
+import { Types } from "mongoose";
+
+export enum Role {
+  SUPER_ADMIN = "SUPER_ADMIN",
+  ADMIN = "ADMIN",
+  USER = "USER",
+  GUIDE = "GUIDE",
+}
+
+/// Auth Providers
+//> Email, Password
+//> Google Authentication
+
+export interface IAuthProvider {
+  provider: string; //? "Google", "Credential"
+  providerId: string;
+}
+
+export enum IsActive {
+  ACTIVE = "ACTIVE",
+  INACTIVE = "INACTIVE",
+  BLOCKED = "BLOCKED",
+}
+
+export interface IUser {
+  name: string;
+  email: string;
+  password?: string;
+  phone?: string;
+  picture?: string;
+  address?: string;
+  isDeleted?: boolean;
+  isActive?: IsActive;
+  isVerified?: boolean;
+  role: Role;
+  auths: IAuthProvider[];
+  bookings?: Types.ObjectId[];
+  guides?: Types.ObjectId[];
+}
