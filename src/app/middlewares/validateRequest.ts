1d0509ee178c508400023dc8ec039c27a787c293
rownakabdullahomi <rownakabdullahomi@gmail.com>
2025-07-11 17:31:55 +0600

    createUser schema and middleware for validation of user creation

---
+++ src/app/middlewares/validateRequest.ts
@@ -0,0 +1,13 @@
+import { AnyZodObject } from "zod";
+import { NextFunction, Request, Response } from "express";
+
+export const validateRequest =
+  (zodSchema: AnyZodObject) =>
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      req.body = await zodSchema.parseAsync(req.body);
+      next();
+    } catch (error) {
+      next(error);
+    }
+  };
