e097676037d29405707ff53a53ab979459383c67
rownakabdullahomi <rownakabdullahomi@gmail.com>
2025-07-14 22:28:08 +0600

    google strategy created for google auth using passport

--- src/app.ts
+++ src/app.ts
@@ -4,10 +4,18 @@ import { router } from "./app/routes/routes";
 import { globalErrorHandler } from "./app/middlewares/globalErrorHandler";
 import notFound from "./app/middlewares/notFound";
 import cookieParser from "cookie-parser";
+import passport from "passport";
+import expressSession from "express-session";
 
 
 const app = express();
-
+app.use(expressSession({
+  secret: "Your secret",
+  resave: false,
+  saveUninitialized: false
+}))
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(cookieParser());
 app.use(express.json());
 app.use(cors());
