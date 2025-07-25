b832ae4912de3dbb71c262df5ecd939033ab50a1
rownakabdullahomi <rownakabdullahomi@gmail.com>
2025-07-25 20:55:05 +0600

    multiple image delete done

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -8,59 +8,66 @@ import { handleCastError } from "../helpers/handleCastError";
 import { handleZodError } from "../helpers/handleZodError";
 import { handleValidationError } from "../helpers/handleValidationError";
 import { TErrorSources } from "../interfaces/error.types";
+import { deleteImageFromCLoudinary } from "../config/cloudinary.config";
 
-
-
-export const globalErrorHandler = (
+export const globalErrorHandler = async (
   error: any,
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
-
-  if(envVars.NODE_ENV === "development"){
+  if (envVars.NODE_ENV === "development") {
     console.log(error);
+  }
+
+  console.log({ file: req.files });
+  if (req.file) {
+    await deleteImageFromCLoudinary(req.file.path);
+  }
+
+  if (req.files && Array.isArray(req.files) && req.files.length) {
+    const imageUrls = (req.files as Express.Multer.File[]).map(
+      (file) => file.path
+    );
+
+    await Promise.all(imageUrls.map((url) => deleteImageFromCLoudinary(url)));
   }
 
   let statusCode = 500;
   let message = `Something went wrong!!`;
-  let errorSources: TErrorSources[] = [ ]
+  let errorSources: TErrorSources[] = [];
 
-  /// Duplicate Error 
-  if(error.code === 11000){
-
-    const simplifiedError = handleDuplicateError(error)
+  /// Duplicate Error
+  if (error.code === 11000) {
+    const simplifiedError = handleDuplicateError(error);
     statusCode = simplifiedError.statusCode;
-    message= simplifiedError.message
+    message = simplifiedError.message;
   }
-  /// Cast Error 
-  else if(error.name === "CastError"){
-    const simplifiedError = handleCastError(error)
+  /// Cast Error
+  else if (error.name === "CastError") {
+    const simplifiedError = handleCastError(error);
     statusCode = simplifiedError.statusCode;
-    message= simplifiedError.message
+    message = simplifiedError.message;
   }
-  /// Mongoose Validation Error 
-  else if(error.name === "ZodError"){
-    const simplifiedError = handleZodError(error)
+  /// Mongoose Validation Error
+  else if (error.name === "ZodError") {
+    const simplifiedError = handleZodError(error);
     statusCode = simplifiedError.statusCode;
-    message= simplifiedError.message;
+    message = simplifiedError.message;
     errorSources = simplifiedError.errorSources as TErrorSources[];
   }
-  /// Validation Error 
-  else if(error.name === "ValidationError"){
-    const simplifiedError = handleValidationError(error)
+  /// Validation Error
+  else if (error.name === "ValidationError") {
+    const simplifiedError = handleValidationError(error);
     statusCode = simplifiedError.statusCode;
     errorSources = simplifiedError.errorSources as TErrorSources[];
-    message= simplifiedError.message
-  }
-
-  else if(error instanceof AppError){
-    statusCode = error.statusCode
-    message = error.message
-  }
-  else if (error instanceof Error){
+    message = simplifiedError.message;
+  } else if (error instanceof AppError) {
+    statusCode = error.statusCode;
+    message = error.message;
+  } else if (error instanceof Error) {
     statusCode = 500;
-    message = error.message
+    message = error.message;
   }
 
   res.status(statusCode).json({
